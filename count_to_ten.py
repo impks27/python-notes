e4c9a6ef2f121ad8df56a0d6af417716fa8fcd75
Pradeep Kumar Singh <raj.ratedr001@gmail.com>
2018-11-27 00:23:36 +0530

    update

--- count_to_ten.py
+++ count_to_ten.py
@@ -8,3 +8,22 @@ while count < 11:
 # Code block 2
 if count == 11:
     print('Counting complete.')
+# Code block 3
+print('for loop')
+list = ['a','b','c','d']
+for x in list:
+   if x == 'c':
+    continue
+   print(x)
+
+print('for loop range(6)')
+for x in range(6):
+    print(x)
+
+print('for loop range(2,8)')
+for x in range(2,8):
+    print(x)
+
+print('for loop range(2,30,3)')
+for x in range(2,30,3):
+    print(x)
